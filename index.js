6dc383f114d4eaac92abef86286340b8993c3a29
Timothy <65453736+timothyvilla@users.noreply.github.com>
2024-04-02 01:56:13 +0800

    index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Playing}],
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
     status: 'dnd',
   });
 
