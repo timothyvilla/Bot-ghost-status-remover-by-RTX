3ea071e65a6bcdab45212b661a2d31e94c2da727
Timothy <65453736+timothyvilla@users.noreply.github.com>
2024-04-02 01:14:28 +0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Playing","with Valentine"];
 
 
 let currentIndex = 0;
